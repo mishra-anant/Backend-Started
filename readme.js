b5ef433ac17c75ade6b551e115e686d38b0d2d99
mishra-anant <mishra.anant.hbtu2k23@gmail.com>
2025-03-25 20:20:33 +0530

    Backend Started

---
+++ readme.js
@@ -0,0 +1,53 @@
+/* NODE. js is a javascript runtime built on Google's open-source V8 javascript engine*/
+
+/* Node js is just another javascript runtime.It's just like a container like an environment 
+in which a program written in javascript can be executed but outside of any browser */
+
+/* Who actually does execute the code if not the browser and thats where v8  engine developed
+by google comes into play.Thhats exactly where javascript course will be parsed and run in
+NODEJS. */
+
+/* JAVASCRIPT ON THE SERVER! 
+Now we have the javascript outside of the browser in a kind of stand alone environment with NODE.JS
+we can do many different things with javascript that were completely impossible before 
+1)accessing the file system
+2)better networking capabilities with Node.js
+and all these factors together gives us a perfect condition  to use NODE.JS as a web server meaning 
+that we can finally use javascript on the server-side of web development in order to build
+FAST,HIGHLY SCALABLE NETWORK APPLICATIONS(BACK-END) for powering the backend.
+*/
+
+/* use cases for NODE.JS and why do we use it 
+1)Node.js applications are so fast and so scalable because NODE.js is single-threded,based
+on event driven ,non-blocking I?O model which makes node.js very light weight and efficient.
+
+2)Perfect for building fast and scalable data intensive apps.
+
+This makes node.js a perfect fit for building a perfect fit for building all different kinds 
+of applications like:-
+1)API with database behindit(preferably NOSQQL)
+2)Data streaming(Think Youtube)(Netflix)
+3)Real-time chat applications
+4)Server side web applications where the entire content is simply generated right on the server.
+ */
+
+/* APPS THAT WE SHOULD NOT BUILD WITH NODE.JS 
+1)Applications with heavy server side processing(cpu- intensive) like heavy image manipulation
+,video conversions ,files compression etc.
+*/
+
+//Another great reason for using node is hat there is a huge library of open source packages or modules available everyone
+//for free.Its called NPM.
+
+/* Huge active community */
+
+//underscore _ saves the previous result
+
+/* with node js we can do all kinds of amazing things tht we canot do with javascript in the browser for eg 
+reading files with javascript in the browser from the file system */
+
+/* In order to do that we need to use a node module.So nodejs is really built around this concept
+of modules where all kinds of additional functionality are stored in a module.And in the case for reading files
+inside the fs module */
+
+/*How to open up these modules and how to use them ? :- we use require in our code */
